c3e2bff38840170c09e0de4f6d7c583657f5c6d4
JuliaBessonova <bessonova.jul@gmail.com>
2018-05-31 21:46:53 +0300

    Добавлен тест проверки удаления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -29,6 +29,21 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        # approve deletion
+        wd.switch_to_alert().accept()
+        # go back to home page
+        self.back_to_home_page()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def back_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
