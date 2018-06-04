ca3dc7cde946f8e7ee52d58b18b29b3e9914d21d
JuliaBessonova <bessonova.jul@gmail.com>
2018-06-04 23:47:17 +0300

    Выполнено задание 9

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,6 @@
-
+from model.contact import Contact
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", address="address", phone="81111111111", email="test@test.com"))
     app.contact.delete_first_contact()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,4 +1,6 @@
 from model.contact import Contact
 
 def test_change_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", address="address", phone="81111111111", email="test@test.com"))
     app.contact.change(Contact(firstname="Joseph", lastname="Stalin", address="New York", phone="82222222222", email="qwe@qwe.com"))
