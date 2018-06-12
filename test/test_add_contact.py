d1b583012355ab3544790636ac612578fd30f8ef
JuliaBessonova <bessonova.jul@gmail.com>
2018-06-13 00:57:53 +0300

    реализованы проверки сравнения списков при добавлении групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,15 +3,20 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", address="address", phone="81111111111", email="test@test.com"))
+    contact = Contact(firstname="Ivan", lastname="Ivanov", address="address", phone="81111111111", email="test@test.com")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
 def test_add_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(firstname="", lastname="", address="", phone="", email=""))
+    contact = Contact(firstname="", lastname="", address="", phone="", email="")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
-
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
