945facc4e1436d9d5446dda23df62c566c1e6edb
JuliaBessonova <bessonova.jul@gmail.com>
2018-06-13 01:09:20 +0300

    реализованы проверки сравнения списков при модификации групп и контактов. Выполнено задание 11

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,6 +4,10 @@ def test_change_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", address="address", phone="81111111111", email="test@test.com"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.change_first_contact(Contact(firstname="Joseph", lastname="Stalin", address="New York", phone="82222222222", email="qwe@qwe.com"))
+    contact = Contact(firstname="Joseph", lastname="Stalin", address="New York", phone="82222222222", email="qwe@qwe.com")
+    contact.id = old_contacts[0].id
+    app.contact.change_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
