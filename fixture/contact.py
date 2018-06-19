783a221d224f8da34e1534eae9fb0e20d9a4da70
JuliaBessonova <bessonova.jul@gmail.com>
2018-06-19 23:29:31 +0300

    реализованы проверки всех полей контакта на главной странице

--- fixture/contact.py
+++ fixture/contact.py
@@ -111,9 +111,10 @@ class ContactHelper:
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 lastname = element.find_element_by_xpath(".//td[2]").text
                 firstname = element.find_element_by_xpath(".//td[3]").text
+                address = element.find_element_by_xpath(".//td[4]").text
                 all_phones = cells[5].text
                 all_emails = cells[4].text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, address=address, id=id,
                                                   all_phones_from_home_page=all_phones, all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
 
@@ -122,6 +123,7 @@ class ContactHelper:
         self.open_edit_form_by_index(index)
         firstname = wd.find_element_by_name("firstname").get_attribute("value")
         lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
         homephone = wd.find_element_by_name("home").get_attribute("value")
         workphone = wd.find_element_by_name("work").get_attribute("value")
@@ -130,7 +132,7 @@ class ContactHelper:
         email = wd.find_element_by_name("email").get_attribute("value")
         email2 = wd.find_element_by_name("email2").get_attribute("value")
         email3 = wd.find_element_by_name("email3").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id=id,
+        return Contact(firstname=firstname, lastname=lastname, address=address, id=id,
                        homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone,
                        email=email, email2=email2, email3=email3)
 
