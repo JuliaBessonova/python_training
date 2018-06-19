a7f14244de2a4956276d1239b98a4ae8999e12b5
JuliaBessonova <bessonova.jul@gmail.com>
2018-06-19 23:36:29 +0300

    выполнено задание 14

--- test/test_data_on_home_page.py
+++ test/test_data_on_home_page.py
@@ -1,8 +1,11 @@
 import re
+from random import randrange
 
 def test_data_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
