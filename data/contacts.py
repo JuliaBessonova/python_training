5ec69fd4fc40e8256afe3368f7e988f44b8e3d93
JuliaBessonova <bessonova.jul@gmail.com>
2018-06-25 23:59:48 +0300

    создана динамическая генерация тестов создания групп и контактов

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="name1", lastname="lastname1", address="address", mobilephone="81111111111", workphone="82222222222",
+            email="test1@test.com", email2="test2@test.com", email3="test3@test.com", secondaryphone="83333333333")
+]
+
