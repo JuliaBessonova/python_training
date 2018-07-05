e96a3cea807d146167931c3c2842d800985e9f06
JuliaBessonova <bessonova.jul@gmail.com>
2018-07-06 00:12:43 +0300

    добавлены отключаемые проверки проверки списков групп и контактов из БД и ui

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,7 +1,7 @@
 from model.contact import Contact
 import random
 
-def test_change_first_contact(app, db):
+def test_change_first_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", address="address", homephone="81111111111", email="test@test.com"))
     old_contacts = db.get_contact_list()
@@ -9,9 +9,11 @@ def test_change_first_contact(app, db):
     #index = randrange(len(old_contacts))
     contact = Contact(firstname="Joseph", lastname="Stalin", address="New York", homephone="82222222222", email="qwe@qwe.com")
     #contact.id = old_contacts[index].id
-    app.contact.change_contact_by_id(mcontat.id, contact)
+    app.contact.change_contact_by_id(mcontact.id, contact)
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     new_contacts = db.get_contact_list()
     #old_contacts[index] = contact
     #assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 import random
 
-def test_modify_group_name(app, db):
+def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -14,6 +14,8 @@ def test_modify_group_name(app, db):
     assert len(old_groups) == len(new_groups)
     #old_groups[index] = group
     #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 #def test_change_first_group_header(app):
